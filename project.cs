ea57f45634a55461f74f85f9be965a04b5d12d62
William8500 <62640433+William8500@users.noreply.github.com>
2020-04-25 23:02:08 +0800

    Update project.cs

--- project.cs
+++ project.cs
@@ -1,80 +1,145 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 using System.Text;
+using System.Linq;
 
 public class HangMan : MonoBehaviour
 {
-    string[] words;
+     public Text messageText;
+     public InputField guessInput;
+     public Text systemMessage;
+     public string wordtoGuess;
+     public string hiddenWord;
+     //Player chance can change
+     public int playerLives = 10;
+     public Text livesWindow;
+     public Text showWords;
+     public Text wordsHint;
+     public Button submitButton;
+     public Text wordsWrongShow;
 
-    public int Lives = 10; 
-    private string wordsInput = "";
-    private string wordsGuess = "";
-    private string wordsShows = "";
 
-    void wordsInput ()
+     
+     //Words used (can put in more)
+     string[] wordUsed = 
+        {
+            "Chair",
+            "Table",
+            "Bench",
+            "Spoon",
+            "Choir",
+            "Mouse",
+            "Phone",
+            "Money"
+        };
+    //Random generate words
+    void wordsArray()
     {
-        words = new int [5];
-        words[0] = chair;
-        words[1] = table;
-        words[2] = bench;
-        words[3] = spoon;
-        words[4] = choir;
+       System.Random random = new System.Random();
+       int wordChoose = random.Next(wordUsed.Length);
+       string wordPick = wordUsed[wordChoose];
+       
+       char[] chars = wordPick.ToCharArray();
+       for (int i=0; i<chars.Length; i++)
+       {
+           chars[i] = '_';
+       }
+
+        string wordLastPick = new string (chars);
         
-    }
-
+        string hintWords = wordsHint.text;
+        wordsHint.text = "Word Length: " + wordLastPick.Length;
     
-    
-    private void OnScreen ()
-    {   
-        string secretWords = wordsInput[0];
 
-        int secretWords = secretWords.Length;
-        Console.Write("Your secret word is:");
-        for (int i=0; i<secretWords; i++)
-                Console.Write("*");
-            Console.Write();
-
-
-        while (true)
+        char letter = guessInput.text.ToCharArray()[0];
+        for (int i=0; i<wordPick.Length; i++)
         {
-           for (int i = 0; i < wordsShows.Length; i++) 
-            {
-                if (i < secretWord.Length && i < choice.Length)
-                {
-                    if (wordsInput[i] == words[i])
-                    Console.Write(words[i]);
-                    else
-                    Console.Write("*");
-                }
-                else 
-                Console.Write("*");
-            }
-            Console.WriteLine();
+            if (wordPick[i] == letter)
+                chars[i] = letter;
         }
 
+        hiddenWord = wordLastPick;
+        wordtoGuess = wordPick;
+    }
 
+
+    //Print words to guess
+    void wordsShows()
+    {
+        string plWords = showWords.text;
+        showWords.text = hiddenWord;
+    }
+
+    //Print wrong words
+    void wordsWrong()
+    {
+        string plWrong = wordsWrongShow.text;
+        wordsWrongShow.text = "Wrong Letter: ";
+        if(playerLives <= 0)
         {
-          if (Lives >0 )
-           {
-              Console.write("Try Again");
-           }
-          else
-          {
-             chanceGuess = true;
-
-         }
-
-         if (chanceGuess = true)
-         {
-             Console.Write("You lose");
-         }
-         else
-            {
-                Console.Write("Correct Answer");
-            }
+            wordsWrongShow.gameObject.SetActive(false);
         }
     }
 
+    //Print chance left
+    void Lives()
+    {
+        string plLives = livesWindow.text;
+        livesWindow.text = "Chance:" + playerLives;
+        if(playerLives <= 0)
+        {
+            livesWindow.gameObject.SetActive(false);
+        }
+    }
+
+    //Message indicate player guess right or wrong
+    public void GetGuess()
+    {
+        string guessString = guessInput.text;
+        if (playerLives>0)
+        {
+            if (guessString == wordtoGuess)
+        {
+            systemMessage.text = "You Guess it right";
+            playerLives =11 ;
+        }
+        else 
+            systemMessage.text = "Try again";
+            playerLives--;
+        }
+        
+
+        if (playerLives <= 0)
+        {
+            systemMessage.text = "Game Over\n\nThe words is " + wordtoGuess + "\n\nTry hard next time";
+            GameOver();
+        }
+
+    }
+
+    //Destroy buttons and text object
+    void GameOver()
+    {
+        guessInput.gameObject.SetActive(false);
+        submitButton.gameObject.SetActive(false);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       wordsArray();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       wordsShows();
+       Lives();
+       wordsWrong();
+    }
 }
